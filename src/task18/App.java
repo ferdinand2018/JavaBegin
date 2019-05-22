221e26b1057a6795fa8d581f78fe20529f4cbdfd
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-05-22 18:59:18 +0300

    Исправлено ДЗ 18

--- src/task18/App.java
+++ src/task18/App.java
@@ -3,29 +3,33 @@ package task18;
 import java.io.*;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //Выводим содержимое файла
         String s;
+        int countS = 0;
         try(BufferedReader br = new BufferedReader(new FileReader("src/task18/test.txt"))){
             while ((s = br.readLine()) != null){
                 System.out.println(s);
+                countS++;
             }
         }catch(IOException e){
             System.out.println("Ошибка ввода-вывода: " + e);
         }
+        System.out.println("Кол-во строк в исходном файле: " + countS);
 
         //Редактируем файл
         String str;
+        int countStr = 1;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите stop, чтобы закончить");
+        System.out.println("Отредактируйте файл");
         try(FileWriter fw = new FileWriter("src/task18/test.txt")){
             do{
-                System.out.println(": ");
+                System.out.print(": ");
                 str = br.readLine();
-                if (str.compareTo("stop") == 0) break;
                 str = str + "\r\n";
                 fw.write(str);
-            } while (str.compareTo("stop") != 0);
+                countStr++;
+            } while (countStr <= countS);
         }catch(IOException e){
             System.out.println("Ошибка ввода-вывода: " + e);
         }
